5175889072caf4c23579cc9da7db89b21ba250e2
ron7116 <farjilul.rony@gmail.com>
2025-05-22 17:20:51 +0600

    my lisiting page added

--- src/pages/MyListing.jsx
+++ src/pages/MyListing.jsx
@@ -1,12 +1,47 @@
-import { use } from "react";
+import { use, useState } from "react";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 import { AuthContext } from "../context/AuthContext";
 
 const MyListing = () => {
   const { allUsers } = useLoaderData();
   const { user } = use(AuthContext);
-  const sameUserData = allUsers.filter((item) => item.email == user.email);
-  console.log(sameUserData);
+  const sameUserDataFilter = allUsers.filter(
+    (item) => item.email == user.email
+  );
+  const [sameUserData, setSameUserData] = useState(sameUserDataFilter);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+              const remainingData = sameUserDataFilter.filter(
+                (value) => value._id !== id
+              );
+              setSameUserData(remainingData);
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="p-6 overflow-x-auto">
       <h1 className="text-2xl font-bold mb-4 text-center">My Listings</h1>
@@ -35,7 +70,10 @@ const MyListing = () => {
                 <button className="btn btn-sm btn-outline btn-info">
                   Update
                 </button>
-                <button className="btn btn-sm btn-outline btn-error">
+                <button
+                  onClick={() => handleDelete(item._id)}
+                  className="btn btn-sm btn-outline btn-error"
+                >
                   Delete
                 </button>
               </td>
